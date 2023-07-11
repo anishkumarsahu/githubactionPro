dc7f011db7fb99cb8753a1a88a6490d3c5108e71
anishkumarsahu <anishkumarsahu03@gmail.com>
2023-07-11 14:59:12 +0530

    added test, coverage, and workflow

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(
+            title='Test',
+            author='Test',
+            slug='test'
+        )
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(self.blog), 'Test')
